fa06b6de22bc2fc4d22747e2c3d73fe604a0d32f
Dmitry <flabbergasted242@gmail.com>
2025-06-03 23:15:02 +0300

    Initial Commit

---
+++ MyPetClinics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPetClinics
+//
+//  Created by Dmitry Dmitry on 3.6.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
